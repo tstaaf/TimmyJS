6c64a051892eeb23a4d01c1fecbce6ba3bdf5c3d
tstaaf <timmyjs@hotmail.com>
2018-04-03 15:11:06 +0200

    Added feature
    added feature to list specific genre

--- queries.sql
+++ queries.sql
@@ -1,10 +1,6 @@
--- Fråga 1: Vilka filmer som firman äger, inklusive data om filmen.
+-- Fråga 2: Vilka filmer som finns i en viss genre.
 
-SELECT m.movieID, movieName, `Year`, directorsID Directors, group_concat(DISTINCT mah.ActorsID) Actors, GenreID Genre 
-FROM movies m
-
-INNER JOIN movies_has_actors mah ON m.movieID = mah.movieID
-
+SELECT movieName, g.`Name` FROM movies m
 INNER JOIN movies_has_genre mhg ON m.movieID = mhg.movieID
-
-GROUP BY m.movieID;
+INNER JOIN genre g ON mhg.GenreID = g.GenreID
+WHERE g.`Name`= 'Drama';
