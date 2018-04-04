e12995b2c444a894802cda7a1ec46d067d283fc7
tstaaf <timmyjs@hotmail.com>
2018-04-04 12:21:46 +0200

    Fixed errors
    Fixed full name not being displayed

--- queries.sql
+++ queries.sql
@@ -1,10 +1,16 @@
 -- Fråga 1: Vilka filmer som firman äger, inklusive data om filmen.
 
-SELECT m.movieID, movieName, `Year`, directorsID Directors, group_concat(DISTINCT mah.ActorsID) Actors, GenreID Genre 
+SELECT m.movieID, movieName, `Year`, GROUP_CONCAT(distinct d.FirstName, ' ', d.LastName) Director, GROUP_CONCAT(distinct a.FirstName, ' ', a.LastName) Actors, g.`Name` Genre
 FROM movies m
 
 INNER JOIN movies_has_actors mah ON m.movieID = mah.movieID
 
+INNER JOIN actors a ON mah.ActorsID = a.ActorsID
+
 INNER JOIN movies_has_genre mhg ON m.movieID = mhg.movieID
 
-GROUP BY m.movieID;
+INNER JOIN genre g ON mhg.GenreID = g.GenreID
+
+INNER JOIN directors d ON m.directorsID = d.DirectorsID
+
+GROUP BY m.movieID
