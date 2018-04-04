869b0cdd509fd28886c18b99dafd7a083cd97fbb
tstaaf <timmyjs@hotmail.com>
2018-04-04 13:40:30 +0200

    Added function
    added function to list all rented movies, who rented them, and which employee  rented it out

--- queries.sql
+++ queries.sql
@@ -2,23 +2,26 @@
 
 SELECT m.movieID, movieName, `Year`, GROUP_CONCAT(distinct d.FirstName, ' ', d.LastName) Director, GROUP_CONCAT(distinct a.FirstName, ' ', a.LastName) Actors, g.`Name` Genre
 FROM movies m
-
 INNER JOIN movies_has_actors mah ON m.movieID = mah.movieID
-
 INNER JOIN actors a ON mah.ActorsID = a.ActorsID
-
 INNER JOIN movies_has_genre mhg ON m.movieID = mhg.movieID
-
 INNER JOIN genre g ON mhg.GenreID = g.GenreID
-
 INNER JOIN directors d ON m.directorsID = d.DirectorsID
-
 GROUP BY m.movieID;
-
 
 -- Fråga 2: Vilka filmer som finns i en viss genre.
 
-SELECT movieName, g.`Name` FROM movies m
+SELECT movieName, g.`Name` 
+FROM movies m
 INNER JOIN movies_has_genre mhg ON m.movieID = mhg.movieID
 INNER JOIN genre g ON mhg.GenreID = g.GenreID
-WHERE g.`Name`= 'Drama';
+WHERE g.`Name`= 'Adventure';
+
+-- Fråga 3 : Vilka filmer som är uthyrda, vem som hyrde dem (kund) och vem som hyrde ut dem (anställd).
+
+SELECT m.movieName, e.employeeID, GROUP_CONCAT(c.FirstName,' ' , c.LastName) Renter, m.rented 
+FROM movies m
+INNER JOIN customers c ON c.customerID = m.customerID
+INNER JOIN employees e ON e.employeeID = m.employeeID
+GROUP BY m.movieName
+HAVING m.rented = 1;
