2056b69afb80a29102c77a73e1886dd55c3b6264
tstaaf <timmyjs@hotmail.com>
2018-04-05 14:13:29 +0200

    base

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `timmyjs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `timmyjs`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: timmyjs
@@ -467,4 +469,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-05 14:07:56
+-- Dump completed on 2018-04-05 14:12:33
