2135a62dd06af78fbc99a7557945db3075cd8a00
tstaaf <timmystaaf@gmail.com>
2018-05-03 12:32:56 +0200

    fixed naming

--- queries.sql
+++ queries.sql
@@ -1,22 +1,22 @@
 -- Fråga 1: Vilka filmer som firman äger, inklusive data om filmen.
 
-SELECT * FROM ListAllMovies;
+SELECT * FROM listallmovies;
 
 -- Fråga 2: Vilka filmer som finns i en viss genre.
 
-SELECT * FROM ListAdventureGenre;
+SELECT * FROM listadventuregenre;
 
 -- Fråga 3 : Vilka filmer som är uthyrda, vem som hyrde dem (kund) och vem som hyrde ut dem (anställd).
 
-SELECT * FROM RentalStatus;
+SELECT * FROM rentalstatus;
 
 -- Fråga 4: Vilka filmer som har gått över tiden, dvs filmer som inte har blivit återlämnade trots att de borde vara det, tillsammans med namnet på kunden som har hyrt den.
 
-SELECT * FROM MovieOverdue;
+SELECT * FROM movieoverdue;
 
 -- Fråga 5: En lista över alla anställda och hur många filmer varje anställd har hyrt ut.
 
-SELECT * FROM ListAllEmployees;
+SELECT * FROM listallemployees;
 
 -- Fråga 7: En Stored Procedure som ska köras när en film lämnas ut. Ska alltså sätta filmen till uthyrd, vem som hyrt den osv.
 
